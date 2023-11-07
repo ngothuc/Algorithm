632f064a21f2f387f1b83a8f400f6a05d675f122
Ngo Van Thuc <ngothuc0704@gmail.com>
2023-11-07 16:57:22 +0700

    Learning myself

--- Max_Distance_Subsequence.cpp
+++ Max_Distance_Subsequence.cpp
@@ -1,44 +1,57 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
+#define loop(i, a, b) for(int i=a; i<=b; i++)
+#define N 100001
+
 using namespace std;
 
-bool check(int x[], int N, int C, int mid) {
-    int cnt = 1, prev = x[0];
-    for (int i = 1; i < N; i++) {
-        if (x[i] - prev >= mid) {
-            cnt++;
-            prev = x[i];
-        }
-    }
-    return cnt >= C;
+int t, n , c, ans;
+int x[N];
+int Left, Right, Mid;
+
+bool Check(int Mid)
+{
+	int cnt = 1;
+	int tmp = x[1];
+	loop(i, 2, n)
+	{
+		if(x[i] - tmp >= Mid)
+		{
+			cnt++;
+			tmp = x[i];
+		}
+	}
+	return cnt >= c;
 }
 
-int main() {
+
+int main()
+{
 	ios_base::sync_with_stdio(0);
 	cin.tie(NULL); cout.tie(NULL);
-	//freopen("ngothuc.txt", "r", stdin);
-    int T;
-    cin >> T;
-    while (T--) {
-        int N, C;
-        cin >> N >> C;
-        int x[N];
-        for (int i = 0; i < N; i++) {
-            cin >> x[i];
-        }
-        sort(x, x + N);
-        
-        int left = 1, right = x[N - 1] - x[0], ans = -1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (check(x, N, C, mid)) {
-                ans = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        cout << ans << endl;
-    }
-    return 0;
+	freopen("ngothuc.txt", "r", stdin);
+	
+	cin >> t;
+	while(t--)
+	{
+		cin >> n >> c;
+		loop(i, 1, n) cin >> x[i];
+		sort(x+1, x+n+1);
+		Left = 1; Right = x[n] - x[1]; ans = INT_MIN;
+		
+		while(Left <= Right)
+		{
+			Mid = (Left + Right) / 2;
+			if(Check(Mid))
+			{
+				ans = Mid;
+				Left = Mid + 1;
+			}else{
+				Right = Mid - 1;
+			}
+		}
+		cout << ans << endl;
+	}
+	
+	return 0;
 }
-
