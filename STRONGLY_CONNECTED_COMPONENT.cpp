3b74ce05d59f0d72bd95f32f1ed0a2ca0d378a18
Ngo Van Thuc <ngothuc0704@gmail.com>
2023-12-13 18:41:24 +0700

    Week 10

--- STRONGLY_CONNECTED_COMPONENT.cpp
+++ STRONGLY_CONNECTED_COMPONENT.cpp
@@ -1,126 +1,66 @@
-#include<bits/stdc++.h>
-
-#define loop(i, a, b) for(int i = a; i <= b; i++)
-#define N 100001
-#define M 1000001
-
+#include <bits/stdc++.h>
 using namespace std;
+#define MAX 100005
 
 int n, m;
-vector<int> E[N];
-vector<int> e[N];
-pair<int, int> f[N];
-int t = 0;
-int level[N], prep[N];
-int visited[N];
+vector<int> A[MAX];
 
-void printEdge() {
-	loop(i, 1, n) {
-		cout << i << ": ";
-		if(E[i].size() == 0) {
-			cout << endl;
-			continue;
-		}
-		for(auto x : E[i]) {
-			cout << x << " ";
-		}
-		cout << endl;
-	}
+int num[MAX], low[MAX], timeDFS;
+bool is_deleted[MAX];
+stack<int> st;
+int components;
+
+
+void input() {
+   ios_base::sync_with_stdio(0);
+   cin.tie(0); cout.tie(0);
+
+//   freopen("ComputeStronglyConnectedComponentsOfDirectedGraphs.txt", "r", stdin);
+
+   cin >> n >> m;
+   int u, v;
+   for (int i=1; i<=m; i++) {
+      cin >> u >> v;
+      A[u].push_back(v);
+   }
 }
 
 void DFS(int u) {
-	stack<int> S;
-	S.push(u); prep[u] = 0;
-	while(!S.empty()) {
-		int v = S.top(); S.pop();
-		if(f[v].second == 0) {
-			f[v].second = ++t;
-			level[prep[v]]--;
-			for(auto x : E[v]) {
-				if(f[x].second == -1) {
-					prep[x] = v; level[v]++;
-					f[x].second = 0;
-					S.push(x);
-				}
-			}
-		}
-		if(level[v] == 0) {
-			f[v].second = ++t;
-			v = prep[v];
-			while(v != 0 && level[v] == 0) {
-				f[v].second = ++t;
-				v = prep[v];
-			}
-		}
-	}
+   st.push(u);
+   num[u] = ++timeDFS;
+   low[u] = num[u];
+
+   for (int& v : A[u]) {
+      if (is_deleted[v]) continue;
+      if (num[v]) low[u] = min(low[u], num[v]);
+      else {
+         DFS(v);
+         low[u] = min(low[u], low[v]);
+      }
+   }
+   if (low[u] == num[u]) {
+      ++components;
+      int v;
+      do {
+         v = st.top(); st.pop();
+         is_deleted[v] = true;
+//         cout << v << " ";
+      }while (v != u);
+//      cout << endl;
+   }
 }
 
-void dfs(int u) {	
-	stack<int> S;
-	S.push(u);
-	while(!S.empty()) {
-		int v = S.top(); S.pop();
-		if(visited[v] == 1) {
-			visited[v] = 2;
-			for(auto x : e[v]) {
-				if(visited[x] == 0) {
-					visited[x] = 1;
-					S.push(x);
-				}
-			}
-		}
-	}
+void solve() {
+   for (int u=1; u<=n; u++) {
+      if (!num[u]) DFS(u);
+   }
+   cout << components;
 }
 
-bool comp(pair<int, int> x, pair<int, int> y) {
-	return x.second > y.second;
+int main()
+{
+   input();
+   solve();
+   return 0;
 }
 
-int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(NULL); cout.tie(NULL);
-	//freopen("ngothuc.txt", "r", stdin);
-	
-	cin >> n >> m;
-	
-	loop(i, 1, n) {
-		f[i].first = i;
-		f[i].second = -1;
-		level[i] = 0;
-		visited[i] = 0;
-	}
-	
-	int u, v;
-	
-	while(m--) {
-		cin >> u >> v;
-		E[u].push_back(v);
-		e[v].push_back(u);
-	}
-
-	//printEdge();
-	
-	loop(i, 1, n) {
-		if(f[i].second == -1) {
-			f[i].second = 0;
-			DFS(i);
-		}
-	}
-	sort(&f[1], &f[n+1], comp);
-	//loop(i, 1, n) cout << f[i].first << " " << f[i].second << endl;
-	
-	int ans = 0;
-	
-	loop(i, 1, n) {
-		if(visited[f[i].first] == 0) {
-			visited[f[i].first] = 1;
-			dfs(f[i].first);
-			ans++;
-		}
-	}
-	
-	if(ans != 151) cout << ans;
-	else cout << ans + 1;
-
-	return 0;
-}
